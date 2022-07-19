5bfce1bc390d64a68008fa6660e48aa2216eeb2a
Chloe Xie <t-xiechloe@microsoft.com>
2022-07-19 11:32:32 -0700

    Added input shape

--- tvm_onnx.py
+++ tvm_onnx.py
@@ -6,16 +6,18 @@ import tvm.relay as relay
 import mlflow
 
 # Load a previously converted model. This model was converted from skl to ONNX with Hummingbird.
-onnx_model = mlflow.onnx.load_model("0\b81d20537a9345cf9126d7c8bdc7a2c6\artifacts\onnx_model")
-ml_model = mlflow.models.Model.load("0\b81d20537a9345cf9126d7c8bdc7a2c6\artifacts\onnx_model")
-input_example =  ml_model.load_input_example("0\b81d20537a9345cf9126d7c8bdc7a2c6\artifacts\onnx_model")
-
+onnx_model = mlflow.onnx.load_model("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
+ml_model = mlflow.onnx.Model.load("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
+input_example =  ml_model.load_input_example("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
+input_shape = input_example.shape
+print(f'input_shape: {input_shape}')
+print(f'input_shape type: {type(input_shape)}')
 # Code below is from TVM ONNX example in their official docs 
 # https://tvm.apache.org/docs/how_to/compile_models/from_onnx.html#compile-the-model-with-relay
 target = "llvm"
 
 input_name = "1"
-shape_dict = {input_name: input_example}
+shape_dict = {input_name: input_shape}
 # Line below throws errors about the input types
 mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
 
